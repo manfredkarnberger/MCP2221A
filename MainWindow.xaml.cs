e4919163e655e22ab68543536e5e7664bd0550ae
Manfred Karnberger <karnberger.manfred@gmail.com>
2025-08-17 22:39:34 +0200

    minor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using mcp2221_dll_m;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using mcp2221_dll_m;
+using System.Diagnostics;
 
 namespace MCP2221A
 {
@@ -21,11 +24,22 @@ namespace MCP2221A
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int DEFAULT_VID = 0x04D8;
+        const int DEFAULT_PID = 0x00DD;
+        static int result = 0;
+        static uint numberOfDevices = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
             String libVersion = MCP2221.M_Mcp2221_GetLibraryVersion();
-            MCP2221.M_Mcp2221_OpenBySN("MCP2221A", 0, out int handle);
+
+            result = MCP2221.M_Mcp2221_GetConnectedDevices(DEFAULT_VID, DEFAULT_PID, ref numberOfDevices);
+            if (result == MCP2221.M_E_NO_ERR)
+            {
+                Debug.WriteLine($"Number of connected devices: {numberOfDevices}");
+            }
         }
     }
 }
